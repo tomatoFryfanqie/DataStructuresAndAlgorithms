c7ad1fd616ce084349394fd8846ae9f4d4e3afc1
deeperBlue <1093369951@qq.com>
2020-10-22 18:25:17 +0800

    ShellSort

---
+++ src/Algorithms/Sort/ShellSort.java
@@ -0,0 +1,23 @@
+package Algorithms.Sort;
+
+/**
+ * @author lzh
+ */
+public class ShellSort {
+
+    public static void shellSort(int[] arr){
+
+        for (int step = arr.length/2; step > 0; step/=2) {
+
+            for (int i = step; i < arr.length; i++) {
+
+                for (int j = i; j >=step && arr[j] < arr[j-step] ; j-=step) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j-step];
+                    arr[j-step] = temp;
+                }
+            }
+        }
+    }
+}
